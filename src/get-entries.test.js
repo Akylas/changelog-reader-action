f6f78e62c0f655c883186b8e1a13c9667d6192d7
Martin Guillon <martin.guillon@akylas.fr>
2022-03-22 10:25:56 +0100

    fix: support angular and other templates

--- src/get-entries.test.js
+++ src/get-entries.test.js
@@ -73,7 +73,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 - README now uses the correct version number in the examples
 
-## [1.0.1] - 2020-02-12
+# [1.0.1] - 2020-02-12
 ### Fixed
 - Remove template's old behavior
 
